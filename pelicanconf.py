ff7bdb4db1b37be89ab599073a977ae619e47aa2
aseom <hm9599@gmail.com>
2016-03-14 02:23:56 +0900

    기본 틀 완성

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,12 +7,20 @@ SITENAME = 'pelican-test'
 SITEURL = ''
 
 PATH = 'content'
+DELETE_OUTPUT_DIRECTORY = True
 
 TIMEZONE = 'Asia/Seoul'
-
 DEFAULT_LANG = 'ko'
 
-# Feed generation is usually not desired when developing
+# URL
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+ARTICLE_URL = '{category}/{slug}/'
+ARTICLE_SAVE_AS = '{category}/{slug}/index.html'
+CATEGORY_URL = 'category/{slug}/'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+
+# Disable feed generation
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
@@ -30,6 +38,3 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
