bd9d858f8fd117b76f2e089bb05fa572bf8ca374
aseom <hm9599@gmail.com>
2016-03-16 02:00:18 +0900

    config 명령어 대신 커밋시 author 넣어봄

--- publish.sh
+++ publish.sh
@@ -8,17 +8,14 @@ COMMIT_MSG="Publish via travis - Build #${TRAVIS_BUILD_NUMBER}"
 # Ensure it is master branch
 if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 1; fi
 
-git config --global user.name "aseom"
-git config --global user.email "hm9599@gmail.com"
 git clone -b gh-pages ${GH_URL} publish
-
 cd publish
+
 git rm -rf --quiet .
 cp -r ../output/* .
-
 # Prevent fail with "nothing to commit"
 echo "#${TRAVIS_BUILD_NUMBER}" > .travis_build
 
 git add -A .
-git commit -m "${COMMIT_MSG}"
+git commit --author"aseom <hm9599@gmail.com>" -m "${COMMIT_MSG}"
 git push origin gh-pages
