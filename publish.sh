e9b94c89beb14f7d7638fa2d5ba7061739f6acf9
aseom <hm9599@gmail.com>
2016-03-15 23:39:33 +0900

    Improve publish script

--- publish.sh
+++ publish.sh
@@ -5,7 +5,8 @@ set -o verbose
 GH_URL="https://${GH_TOKEN}@github.com/aseom/pelican-test.git"
 COMMIT_MSG="Publish via travis - Build #${TRAVIS_BUILD_NUMBER}"
 
-if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 0; fi
+# Ensure it is master branch
+if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 1; fi
 
 git config --global user.name "aseom"
 git config --global user.email "hm9599@gmail.com"
@@ -14,7 +15,9 @@ git clone -b gh-pages ${GH_URL} publish
 cd publish
 git rm -rf --quiet .
 cp -r ../output/* .
-ls -AF --color .
+
+# Prevent fail with "nothing to commit"
+echo "#${TRAVIS_BUILD_NUMBER}" > .travis_build
 
 git add -A .
 git commit -m "${COMMIT_MSG}"
