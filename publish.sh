261497578d823e379ad25ce6eb33a351d802ba85
aseom <hm9599@gmail.com>
2016-03-15 21:21:47 +0900

    Add '--global' not '-g'

--- publish.sh
+++ publish.sh
@@ -5,8 +5,8 @@ GH_URL="https://${GH_TOKEN}@github.com/aseom/pelican-test.git"
 COMMIT_MSG="Publish travis build #${TRAVIS_BUILD_NUMBER}"
 
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    git config -g user.name "aseom-travis"
-    git config -g user.email "hm9599@gmail.com"
+    git config --global user.name "aseom-travis"
+    git config --global user.email "hm9599@gmail.com"
     git clone -b gh-pages ${GH_URL} publish
 
     cd publish
