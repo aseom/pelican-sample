2faba91f0140e11c9d9271f9d5b1925c450cd24d
aseom <hm9599@gmail.com>
2016-03-15 21:17:04 +0900

    git config '-g'

--- publish.sh
+++ publish.sh
@@ -5,15 +5,15 @@ GH_URL="https://${GH_TOKEN}@github.com/aseom/pelican-test.git"
 COMMIT_MSG="Publish travis build #${TRAVIS_BUILD_NUMBER}"
 
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    git config user.name "aseom-travis"
-    git config user.email "hm9599@gmail.com"
+    git config -g user.name "aseom-travis"
+    git config -g user.email "hm9599@gmail.com"
     git clone -b gh-pages ${GH_URL} publish
 
     cd publish
     echo "Test!" > dummyfile
 
-    git status
     git add -A .
+    git status
     git commit -m "${COMMIT_MSG}"
     git push origin gh-pages
 fi
