2ed0a43de7c66a1cfa2c9f7b3633f4ea85991e2e
aseom <hm9599@gmail.com>
2016-03-15 22:30:36 +0900

    Now test pelican build!

--- publish.sh
+++ publish.sh
@@ -5,16 +5,18 @@ set -o xtrace
 GH_URL="https://${GH_TOKEN}@github.com/aseom/pelican-test.git"
 COMMIT_MSG="Publish via travis - Build #${TRAVIS_BUILD_NUMBER}"
 
-if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    git config --global user.name "aseom"
-    git config --global user.email "hm9599@gmail.com"
-    git clone -b gh-pages ${GH_URL} publish
+if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 0; fi
 
-    cd publish
-    echo "Test!" > dummyfile3
+git config --global user.name "aseom"
+git config --global user.email "hm9599@gmail.com"
+git clone -b gh-pages ${GH_URL} publish
 
-    git add -A .
-    git status
-    git commit -m "${COMMIT_MSG}"
-    git push origin gh-pages
-fi
+cd publish
+git rm -rf .
+cp -r ../output .
+ls -AF --color .
+
+git add -A .
+git status
+git commit -m "${COMMIT_MSG}"
+git push origin gh-pages
