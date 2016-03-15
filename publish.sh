6c421cfc791ee93bf4e10472cfed594e84650909
aseom <hm9599@gmail.com>
2016-03-15 22:02:43 +0900

    'bash publish.sh' -> './publish.sh'

--- publish.sh
+++ publish.sh
@@ -3,15 +3,15 @@ set -o errexit
 set -o xtrace
 
 GH_URL="https://${GH_TOKEN}@github.com/aseom/pelican-test.git"
-COMMIT_MSG="Publish travis build #${TRAVIS_BUILD_NUMBER}"
+COMMIT_MSG="Publish via travis - Build #${TRAVIS_BUILD_NUMBER}"
 
 if [ "${TRAVIS_BRANCH}" == "master" ]; then
-    git config --global user.name "aseom-travis"
+    git config --global user.name "aseom"
     git config --global user.email "hm9599@gmail.com"
     git clone -b gh-pages ${GH_URL} publish
 
     cd publish
-    echo "Test!" > dummyfile2
+    echo "Test!" > dummyfile3
 
     git add -A .
     git status
