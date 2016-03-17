c51cebb86b6719cadb8ea4e6b2ee1aaa9ba8d3e3
aseom <hm9599@gmail.com>
2016-03-17 18:20:16 +0900

    Last change

--- tasks.py
+++ tasks.py
@@ -8,9 +8,9 @@ if os.environ.get('TRAVIS'):
         """Deploy via Travis CI
         Raise execption is nonzero exit
         """
-        print("\033[33m*** Start publish...\n\033[0m")
+        print("\n\033[33mStart publish...\033[0m")
         check_call('pelican -s publishconf.py -o output', shell=True)
-        print('\033[33m*** Start deploy...\n\033[0m')
+        print('\n\033[33mStart deploy...\033[0m')
         check_call('chmod +x deploy.sh && ./deploy.sh', shell=True)
 
 @task
