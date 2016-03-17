28d8568d984321c2fe64e6a1f10cfb182cedbddc
aseom <hm9599@gmail.com>
2016-03-17 21:39:35 +0900

    Try fix again: No such file

--- deploy.sh
+++ deploy.sh
@@ -8,14 +8,14 @@ DEPLOY_BRANCH="gh-pages"
 
 # Ensure it is master branch
 if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 1; fi
-
-git config --global user.name "aseom-travis"
-git config --global user.email "hm9599@gmail.com"
-git config --global credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
     
 git clone --depth=5 --branch=${DEPLOY_BRANCH} https://github.com/${DEPLOY_REPO}.git deploy
 cd deploy
+
+git config user.name "aseom-travis"
+git config user.email "hm9599@gmail.com"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
 git rm -rf --quiet .
 cp -r ../output/* .
