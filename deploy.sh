e4e5e4295f00b60f7259da70beca267f0bc30828
aseom <hm9599@gmail.com>
2016-03-17 21:21:36 +0900

    git-credential-store 사용

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,19 @@ set -o errexit
 set -o nounset
 set -o verbose
 
-PUBLISH_REPO="https://${GH_TOKEN}@github.com/aseom/pelican-sample.git"
-PUBLISH_BRANCH="gh-pages"
+DEPLOY_REPO="aseom/pelican-sample"
+DEPLOY_BRANCH="gh-pages"
 
 # Ensure it is master branch
 if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 1; fi
+
+git config --global user.name "aseom-travis"
+git config --global user.email "hm9599@gmail.com"
+git config --global credential.helper "store --file=~/.git-credentials"
+echo "https://${GH_TOKEN}:@github.com" > ~/.git-credentials
     
-git clone --depth=5 --branch=${PUBLISH_BRANCH} ${PUBLISH_REPO} publish
-cd publish
+git clone --depth=5 --branch=${DEPLOY_BRANCH} https://github.com/${DEPLOY_REPO}.git deploy
+cd deploy
 
 git rm -rf --quiet .
 cp -r ../output/* .
@@ -18,7 +23,5 @@ cp -r ../output/* .
 echo "#${TRAVIS_BUILD_NUMBER}" > .travis_build
 
 git add -A .
-git config --global user.name "aseom-travis"
-git config --global user.email "hm9599@gmail.com"
 git commit -m "Publish via travis - Build #${TRAVIS_BUILD_NUMBER}"
 git push origin gh-pages
