b9085da2f0988a6938d09adb9b48c49165ac802b
aseom <hm9599@gmail.com>
2016-03-19 15:54:53 +0900

    deploy시 ssh private key 사용

--- README.md
+++ README.md
@@ -14,3 +14,21 @@
 - Travis CI를 통해 자동으로 static site 생성
 - 생성된 파일들이 `gh-pages` 브랜치로 푸시됨
 - 컨텐츠가 변경될 때마다 다시 반복
+
+## Deploy key
+
+Travis CI를 통해 빌드된 사이트가 `gh-pages`로 푸시되려면 ssh key가 필요.
+
+```Shell
+$ ssh-keygen -t rsa -C "pelican-sample deploy key" -N "" -f deploy_key
+
+$ openssl aes-256-cbc -in deploy_key -out deploy_key.enc -K $(openssl rand -hex 32) -iv $(openssl rand -hex 16) -p
+
+$ travis encrypt DEPLOY_KEY_K=[생성된 key] --add
+$ travis encrypt DEPLOY_KEY_IV=[생성된 iv] --add
+```
+
+public key는 _GitHub 저장소 → Settings → Deploy keys_에 추가.  
+암호화된 private key인 `deploy_key.enc`만 저장소에 커밋.
+
+`DEPLOY_KEY_K`와 `DEPLOY_KEY_IV`는 `.travis.yml`에 암호화되어 저장됨.
--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,21 @@ set -o errexit
 set -o nounset
 set -o verbose
 
-DEPLOY_REPO="aseom/pelican-sample"
+DEPLOY_REPO="git@github.com:aseom/pelican-sample.git"
 DEPLOY_BRANCH="gh-pages"
 
 # Ensure it is master branch
 if [ "${TRAVIS_BRANCH}" != "master" ]; then exit 1; fi
-    
-git clone --depth=5 --branch=${DEPLOY_BRANCH} https://github.com/${DEPLOY_REPO}.git deploy
-cd deploy
 
-git config user.name "aseom-travis"
-git config user.email "hm9599@gmail.com"
-git config credential.helper "store --file=.git/credentials"
-echo "https://${GH_TOKEN}:@github.com" > .git/credentials
+# Add ssh private key for deploy
+openssl aes-256-cbc -d -in deploy_key.enc -out deploy_key -K ${DEPLOY_KEY_K} -iv ${DEPLOY_KEY_IV}
+chmod 600 deploy_key
+eval "ssh-agent -s"
+ssh-add deploy_key
+
+# Clone deploy branch    
+git clone --depth=5 --branch=${DEPLOY_BRANCH} ${DEPLOY_REPO} deploy
+cd deploy
 
 git rm -rf --quiet .
 cp -r ../output/* .
@@ -23,5 +25,7 @@ cp -r ../output/* .
 echo "#${TRAVIS_BUILD_NUMBER}" > .travis_build
 
 git add -A .
+git config user.name "aseom-travis"
+git config user.email "hm9599@gmail.com"
 git commit -m "Publish via travis - Build #${TRAVIS_BUILD_NUMBER}"
 git push origin gh-pages
